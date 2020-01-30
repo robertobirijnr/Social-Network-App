ea263038ada2e42195bf71963f49587213f85d14
robertobirijnr <robertobirijnr@hotmail.com>
2020-01-30 15:35:19 +0000

    create post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
+const Profile = require("../models/profileModel");
+const User = require("../models/userModel");
+const { check, validationResult } = require("express-validator");
+const auth = require("../middlewares/auth");
 
-router.get("/", (req, res) => {
-  res.send("Post Route");
-});
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.send(500).json("Server Error");
+    }
+  }
+);
 
 module.exports = router;
