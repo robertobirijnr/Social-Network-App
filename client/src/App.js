976e3f3962e1b2dc692f5fe0b127d62ff76a77d0
robertobirijnr <robertobirijnr@hotmail.com>
2020-01-31 16:49:41 +0000

    creating pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
-
+import Navbar from "./components/Layout/navBar";
+import Landingpage from "./components/Layout/landingPage";
+import Register from "./components/auth/register";
+import Login from "./components/auth/login";
 const App = () => {
-  return <Fragment> Bob Alaska</Fragment>;
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landingpage} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  );
 };
 
 export default App;
---
+++ client/src/components/Layout/navBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
+const navBar = () => {
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/index">
+          <i className="fa fa-code"></i> DevConnector
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/profiles.html">Developers</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default navBar;
